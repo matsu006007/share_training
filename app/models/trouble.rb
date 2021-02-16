22d4370804f03a5bd941d315988017643bb10509
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-02-16 18:54:20 +0900

    Update trouble.rb

--- app/models/trouble.rb
+++ app/models/trouble.rb
@@ -1,9 +1,7 @@
 class Trouble < ApplicationRecord
   with_options presence: true do
-    validates :troubletitle
+    validates :trouble_title
     validates :trouble_content
-    validates :trouble_data_id
-    validates :another_trouble
   end
     
   belongs_to :user
