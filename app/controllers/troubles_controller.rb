5a92b7dc93abc8c1b09a6edc8526a33a90b91f74
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-02-16 18:54:16 +0900

    Update troubles_controller.rb

--- app/controllers/troubles_controller.rb
+++ app/controllers/troubles_controller.rb
@@ -4,4 +4,20 @@ class TroublesController < ApplicationController
     @troubles = Trouble.includes(:user).order("created_at DESC")
   end
 
+  def new
+    @trouble = Trouble.new
+  end
+
+  def create
+    @trouble = Trouble.new(trouble_params)
+    @trouble.save
+    redirect_to troubles_path
+  end
+
+  private
+
+  def trouble_params
+    params.require(:trouble).permit(:trouble_title, :trouble_content).merge(user_id: current_user.id)
+  end
+
 end
