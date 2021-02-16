5abf84c95ae6c5de1ffa28152f62509ce2b34229
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-02-16 12:09:00 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "top#index"
 
-  resources :trainings, only: [:index, :new, :create]
+  resources :trainings, only: [:index, :new, :create, #:show]
 end
