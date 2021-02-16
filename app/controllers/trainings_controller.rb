9352871f2a1d35ddd2adfa1f1b5c5e9a17f2a443
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-02-16 11:46:01 +0900

    Update trainings_controller.rb

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -4,4 +4,20 @@ class TrainingsController < ApplicationController
     @trainings = Training.includes(:user).order("created_at DESC")
   end
   
+  def new
+    @training = Training.new
+  end
+
+  def create
+    @training = Training.new(training_params)
+    @training.save
+    redirect_to trainings_path
+  end
+
+  private
+
+  def training_params
+    params.require(:training).permit(:title, :content ).merge(user_id: current_user.id)
+  end
+
 end
