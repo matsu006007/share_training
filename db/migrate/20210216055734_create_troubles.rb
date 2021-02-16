cd451302b14ae5850f9b90e57e3bbc67ece3d7d4
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-02-16 18:54:33 +0900

    Update 20210216055734_create_troubles.rb

--- db/migrate/20210216055734_create_troubles.rb
+++ db/migrate/20210216055734_create_troubles.rb
@@ -3,8 +3,6 @@ class CreateTroubles < ActiveRecord::Migration[6.0]
     create_table :troubles do |t|
       t.string :trouble_title,     null: false
       t.text :trouble_content,     null: false
-      t.string :trouble_data_id,   null: false
-      t.string :another_trouble,   null: false
       t.references :user,          foreign_key: true
       t.timestamps
     end
